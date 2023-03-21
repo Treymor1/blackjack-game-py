aae393c46e615de98fc70ec31bf40ceece024f4f
Omar Curahua <omar1991960@hotmail.com>
2023-03-20 23:12:10 -0500

    Create a List with Diccionary

--- blackjack.py
+++ blackjack.py
@@ -10,15 +10,30 @@ def deal(number):
     return cards_dealt
 
 suits = ["spades","clubs","hearts","diamonds"]
-ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+ranks = [{"rank":"A","value":11},
+         {"rank":"2","value":11},
+         {"rank":"3","value":11},
+         {"rank":"4","value":11},
+         {"rank":"5","value":11},
+         {"rank":"6","value":11},
+         {"rank":"7","value":11},
+         {"rank":"8","value":11},
+         {"rank":"9","value":11},
+         {"rank":"10","value":11},
+         {"rank":"","value":11},
+         {"rank":"A","value":11},
+         {"rank":"A","value":11},]
+
 cards = []
 for suit in suits:
     for rank in ranks:
         cards.append([suit,rank])
 
 shuffle()
-cards_dealt = deal(2)
-print(cards_dealt)
+card = deal(1)[0]
+
+rank_dict = {"rank":rank, "value":value}
+print(rank,value)
 
 
 
