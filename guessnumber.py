23ebfaf42d21247b153a551fa91bfce202da529e
Omar Curahua <omar1991960@hotmail.com>
2023-03-22 15:44:31 -0500

    Finish the guess the number

--- guessnumber.py
+++ guessnumber.py
@@ -2,5 +2,14 @@ import random
 
 def guess(x):
     random_number = random.randint(1,x)
-    return random_number
-    
+    guess = 0
+    while guess!= random_number:
+        guess = int(input("Guess the number"))
+        if guess<random_number:
+            print("Too low")
+        elif guess>random_number:
+            print("Too high")
+    print("You Win!")
+
+guess(20)
+
