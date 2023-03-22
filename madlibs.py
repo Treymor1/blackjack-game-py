3c65671e6869b38d153ea785aedb73adec5e96ca
Omar Curahua <omar1991960@hotmail.com>
2023-03-22 15:06:35 -0500

    Create a madlibs

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+adj2 = input("Second adjective: ")
+city = input("City: ")
+madlib = f"Bro you are so {adj}!, but i love you, we stay in {city} together!\
+         Just don't make me feel {adj2}"
+
+print(madlib)
